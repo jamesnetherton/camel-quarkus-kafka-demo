b4d88387bf6b55a3693158673fcaa08c953e8107
James Netherton <jamesnetherton@gmail.com>
2021-11-09 15:12:47 +0000

    Improve secret generation

--- generate-secret.sh
+++ generate-secret.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
+KAFKA_BROKERS=test
 CLIENT_ID=srvc-acct-750d7986-9f79-48f7-ae66-21b5106716a9
 CLIENT_SECRET=4d445d69-ed01-4c5f-b4bb-cc2717e09d15
 
 kubectl delete secret camel-quarkus-kafka-mcs-secret
 
 kubectl create secret generic camel-quarkus-kafka-mcs-secret \
+  --from-literal=KAFKA_BROKERS="${KAFKA_BROKERS}" \
   --from-literal=OAUTH_CLIENT_ID="${CLIENT_ID}" \
   --from-literal=OAUTH_CLIENT_SECRET="${CLIENT_SECRET}"
 
-kubectl get secret camel-quarkus-kafka-mcs-secret -o yaml > src/main/kubernetes/secret.yml
+kubectl label secret camel-quarkus-kafka-mcs-secret "app.kubernetes.io/name"="camel-quarkus-examples-kafka"
 
+kubectl get secret camel-quarkus-kafka-mcs-secret -o yaml > src/main/kubernetes/openshift.yml
 
-
+sed -i "/creationTimestamp/d" src/main/kubernetes/openshift.yml
+sed -i "/namespace/d" src/main/kubernetes/openshift.yml
+sed -i "/resourceVersion/d" src/main/kubernetes/openshift.yml
+sed -i "/uid/d" src/main/kubernetes/openshift.yml
---
+++ src/test/java/org/apache/camel/example/ExchangeRatesIT.java
@@ -0,0 +1,7 @@
+package org.apache.camel.example;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class ExchangeRatesIT extends ExchangeRatesTest {
+}
