8301bf998a97d58c25765601acf515d045ad5d22
James Netherton <jamesnetherton@gmail.com>
2021-11-15 09:59:26 +0000

    Fix generate secret script names

--- generate-secret.sh
+++ generate-secret.sh
@@ -4,16 +4,16 @@ KAFKA_BROKERS=test
 CLIENT_ID=srvc-acct-750d7986-9f79-48f7-ae66-21b5106716a9
 CLIENT_SECRET=4d445d69-ed01-4c5f-b4bb-cc2717e09d15
 
-kubectl delete secret camel-quarkus-kafka-mcs-secret
+kubectl delete secret camel-quarkus-kafka-demo-secret
 
-kubectl create secret generic camel-quarkus-kafka-mcs-secret \
+kubectl create secret generic camel-quarkus-kafka-demo-secret \
   --from-literal=KAFKA_BROKERS="${KAFKA_BROKERS}" \
   --from-literal=OAUTH_CLIENT_ID="${CLIENT_ID}" \
   --from-literal=OAUTH_CLIENT_SECRET="${CLIENT_SECRET}"
 
-kubectl label secret camel-quarkus-kafka-mcs-secret "app.kubernetes.io/name"="camel-quarkus-examples-kafka"
+kubectl label secret camel-quarkus-kafka-demo-secret "app.kubernetes.io/name"="camel-quarkus-kafka-demo"
 
-kubectl get secret camel-quarkus-kafka-mcs-secret -o yaml > src/main/kubernetes/openshift.yml
+kubectl get secret camel-quarkus-kafka-demo-secret -o yaml > src/main/kubernetes/openshift.yml
 
 sed -i "/creationTimestamp/d" src/main/kubernetes/openshift.yml
 sed -i "/namespace/d" src/main/kubernetes/openshift.yml
