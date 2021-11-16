d53df3ee98b9250cf000a2a4cca8079f603facfb
James Netherton <jamesnetherton@gmail.com>
2021-11-16 10:12:50 +0000

    Fix native oauth

--- generate-secret.sh
+++ generate-secret.sh
@@ -7,13 +7,18 @@ if [[ -z "${KAFKA_BROKERS}" ]]; then
   VALID="false"
 fi
 
-if [[ -z "${CLIENT_ID}" ]]; then
-  echo "Must set CLIENT_ID environment variable"
+if [[ -z "${OAUTH_CLIENT_ID}" ]]; then
+  echo "Must set OAUTH_CLIENT_ID environment variable"
   VALID="false"
 fi
 
-if [[ -z "${CLIENT_SECRET}" ]]; then
-  echo "Must set CLIENT_SECRET environment variable"
+if [[ -z "${OAUTH_CLIENT_SECRET}" ]]; then
+  echo "Must set OAUTH_CLIENT_SECRET environment variable"
+  VALID="false"
+fi
+
+if [[ -z "${OAUTH_TOKEN_ENDPOINT_URI}" ]]; then
+  echo "Must set OAUTH_TOKEN_ENDPOINT_URI environment variable"
   VALID="false"
 fi
 
@@ -25,7 +30,8 @@ kubectl delete secret camel-quarkus-kafka-demo-secret
 
 kubectl create secret generic camel-quarkus-kafka-demo-secret \
   --from-literal=KAFKA_BROKERS="${KAFKA_BROKERS}" \
-  --from-literal=OAUTH_CLIENT_ID="${CLIENT_ID}" \
-  --from-literal=OAUTH_CLIENT_SECRET="${CLIENT_SECRET}"
+  --from-literal=OAUTH_CLIENT_ID="${OAUTH_CLIENT_ID}" \
+  --from-literal=OAUTH_CLIENT_SECRET="${OAUTH_CLIENT_SECRET}" \
+  --from-literal=OAUTH_TOKEN_ENDPOINT_URI="${OAUTH_TOKEN_ENDPOINT_URI}"
 
 kubectl label secret camel-quarkus-kafka-demo-secret "app.kubernetes.io/name"="camel-quarkus-kafka-demo"
--- src/main/resources/META-INF/resources/js/app.js
+++ src/main/resources/META-INF/resources/js/app.js
@@ -17,21 +17,27 @@ const myChart = new Chart(
     config
 );
 
-setInterval(function() {
-    $.get("/exchangerates", function(data) {
-        const labels = [];
-        const dataset = [];
-        for (let i = 0; i < data.length; i++) {
-            const date = new Date(data[i].timestamp * 1000);
-            labels.push(formatTimeSegment(date.getHours()) + ":" + formatTimeSegment(date.getMinutes()) + ":" + formatTimeSegment(date.getSeconds()));
-            dataset.push("0." + data[i].value);
-        }
+fetchExchangeRates();
 
-        myChart.data.labels = labels;
-        myChart.data.datasets[0].data = dataset;
-        myChart.update();
-    });
+setInterval(function() {
+  fetchExchangeRates();
 }, 5000);
+
+function fetchExchangeRates() {
+  $.get("/exchangerates", function(data) {
+    const labels = [];
+    const dataset = [];
+    for (let i = 0; i < data.length; i++) {
+        const date = new Date(data[i].timestamp * 1000);
+        labels.push(formatTimeSegment(date.getHours()) + ":" + formatTimeSegment(date.getMinutes()) + ":" + formatTimeSegment(date.getSeconds()));
+        dataset.push("0." + data[i].value);
+    }
+
+    myChart.data.labels = labels;
+    myChart.data.datasets[0].data = dataset;
+    myChart.update();
+  });
+}
 
 function formatTimeSegment(segment) {
     return ('0' + segment).slice(-2);
